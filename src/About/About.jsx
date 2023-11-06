233ab2fc596dae672b939b7e500cfce577e1fb03
Mashhura <mashhura2020@gmail.com>
2023-11-06 13:38:01 +0500

    added showcase component,Discount, and About.I forgot to add each component one by one

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import './About.scss';
+import icon1 from '../../src/assets/img/about1.png';
+import icon2 from '../../src/assets/img/about2.png';
+import { FaArrowRight } from 'react-icons/fa'
+import Tittle from '../Common/Tittle';
+import Button from '../Common/Button';
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+      <div className='container'>
+        <div className="about-img">
+          
+        </div>
+        <div className="about-txt">
+          <Tittle mini='About Us' main='We believe in Organic Foods for Strong Health' />
+          <p>Welcome to the world of natural and organic. Here you can discover the bounty of nature. We have grown on the principles of health, and care. We aim to give our customers a healthy chemical-free meal for perfect nutrition.</p>
+          <div className="flex-row">
+            <img src={icon1} alt="" />
+            <div>
+              <h3>Organic Foods Only</h3>
+              <p>
+                The Product that you ordered will be verified that we have or
+                not if have we will start to move on with the next step.
+              </p>
+            </div>
+          </div>
+          <div className="flex-row">
+            <img src={icon2} alt="" />
+            <div>
+              <h3>Organic Foods Only</h3>
+              <p>
+                The Product that you ordered will be verified that we have or
+                not if have we will start to move on with the next step.
+              </p>
+            </div>
+          </div>
+          <Button color='var(--primary-color)' textcolor='white' border='16px' text='Shop Now'><FaArrowRight/></Button> 
+
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/Discount/Discount.jsx
+++ src/Discount/Discount.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Discount.scss';
+import Tittle from '../Common/Tittle';
+import background from '../assets/img/discount-background (1).png';
+import background2 from '../assets/img/discount-background (2).png';
+import { color } from 'framer-motion';
 
 const Discount = () => {
   return (
-    <div>
-      
+    <div className='discount container'>
+      <div className='discount-wraper '>
+        <div className='discount-card1'>
+          
+          <Tittle  mini='Natural!!' main='Get Garden Fresh Fruits'/>
+        </div>
+        <div className='discount-card2' style={{ backgroundImage: `url(${background2})` }}>
+          <Tittle  mini='Offer!!' main='Get 10% off on Vegetables' />
+        </div>
+      </div>
     </div>
   )
 }
